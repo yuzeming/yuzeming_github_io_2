3a04a26e8d8525ab2bd91515e60d998cf466a1ca
Zeming YU <yuzeming@gmail.com>
2014-07-17 07:54:45 +0800

    add big hw 2.0

--- httpd.py
+++ httpd.py
@@ -1,11 +1,7 @@
-import SimpleHTTPServer
-import SocketServer
-
 PORT = 8000
 
+import SimpleHTTPServer,SocketServer
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-
 print "serving at port", PORT
 httpd.serve_forever()
